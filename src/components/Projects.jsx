ee18888d6324d70da288255150df8b00f2c59a49
manmohan kumar <manmohankumar9508@gmail.com>
2026-08-07 21:19:12 +0530

    fix asset file name

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,38 +1,38 @@
-import { FiExternalLink } from 'react-icons/fi'
+import { FiExternalLink } from "react-icons/fi";
 import CodeHub from "../assets/CodeHub.webp";
 import FoodDelivery from "../assets/food-delivery.webp";
-import VideoConference from "../assets/Videoconferencing.jpg";
+import VideoConference from "../assets/VideoConferencing.jpg";
 import WeatherDashboard from "../assets/Weather.jpg";
 const projects = [
   {
-    title: 'CodeHub-Developers Collaboration Platform',
+    title: "CodeHub-Developers Collaboration Platform",
     description:
-      'CodeHub is a version control and collaboration platform used to host code repositories, track changes, manage projects, and work with teams efficiently.',
-    image:CodeHub,
-    link: '#',
+      "CodeHub is a version control and collaboration platform used to host code repositories, track changes, manage projects, and work with teams efficiently.",
+    image: CodeHub,
+    link: "#",
   },
   {
-    title: 'Food delivery website',
+    title: "Food delivery website",
     description:
-      'A MERN stack-based food ordering platform that allows users to browse menus, add items to the cart, place orders, and track order status with secure authentication.',
-    image:FoodDelivery,
-    link: '#',
+      "A MERN stack-based food ordering platform that allows users to browse menus, add items to the cart, place orders, and track order status with secure authentication.",
+    image: FoodDelivery,
+    link: "#",
   },
   {
-    title: 'Video Conferencing website',
+    title: "Video Conferencing website",
     description:
-      'Built a real-time video conferencing web application using the MERN stack, WebRTC, and Socket.IO. Implemented secure user authentication, video/audio calling, chat messaging, and meeting room management for seamless online communication.',
-    image:VideoConference,
-    link: '#',
+      "Built a real-time video conferencing web application using the MERN stack, WebRTC, and Socket.IO. Implemented secure user authentication, video/audio calling, chat messaging, and meeting room management for seamless online communication.",
+    image: VideoConference,
+    link: "#",
   },
   {
-    title: 'Weather Dashboard',
+    title: "Weather Dashboard",
     description:
-      'Clean weather UI with location search, forecasts, and responsive charts.',
-    image:WeatherDashboard,
-    link: '#',
+      "Clean weather UI with location search, forecasts, and responsive charts.",
+    image: WeatherDashboard,
+    link: "#",
   },
-]
+];
 
 export default function Projects() {
   return (
@@ -70,5 +70,5 @@ export default function Projects() {
         ))}
       </div>
     </section>
-  )
+  );
 }
